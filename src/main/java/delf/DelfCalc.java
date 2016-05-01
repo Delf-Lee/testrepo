a3afa0f5187354a3a4eee6e8c40256e9ab2d179b
delf <delf@delf-PC>
2016-05-02 00:00:14 +0900

    [160501_2359] func2 추가

--- src/main/java/delf/DelfCalc.java
+++ src/main/java/delf/DelfCalc.java
@@ -6,7 +6,11 @@ public class DelfCalc {
 	public void add(int a, int b) {
 		result = a + b;
 	}
-	
+
+	public void sub(int a, int b) {
+		result = a - b + 2 + 3;
+	}
+
 	public int getResult() {
 		return result;
 	}
